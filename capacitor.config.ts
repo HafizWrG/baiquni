28daa10dbce1a21ca44e29e21fb65811a4aabb63
HafizWrG <132629477+HafizWrG@users.noreply.github.com>
2026-02-16 07:17:53 +0800

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,8 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.wuregstore.app',
+  appName: 'WuregStore',
+  webDir: 'out'
+};
+export default config;
